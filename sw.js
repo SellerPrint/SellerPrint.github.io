acd6ea366a7bbe1935d37a04243e2d4ceb676151
SellerPrint <tretisgame999@gmail.com>
2025-10-18 09:25:00 +0000

    Update sw.js -0

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10034157
+    "zoneId": 10034184
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
