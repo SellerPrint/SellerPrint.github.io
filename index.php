6a88de79b347111e9203b3c49d1d926c26a21109
SellerPrint <tretisgame999@gmail.com>
2025-10-30 10:34:35 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,10 @@
 if(isset($_GET['check']) && !empty($_GET['check']) && strlen($_GET['check']) >= 10 )
     die(md5('magenet.com'));
 ?>
+
+<?php
+define('_MN_USER', 'fe4f54d3a225a777db39b8e5d134e0321dc6c3bb');
+require_once($_SERVER['DOCUMENT_ROOT'].'/'._MN_USER.'/magenet.php');
+$magenet = new Magenet();
+echo $magenet->getLinks();
+?>
