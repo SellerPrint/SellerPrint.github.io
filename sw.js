6fe4a3904af95fc509ecbd2642658ae9ab5f9517
SellerPrint <tretisgame999@gmail.com>
2026-02-01 08:24:10 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,10 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10055383
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
