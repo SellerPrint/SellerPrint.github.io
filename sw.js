29d23dad5cf082781514be4cde0b02a2f320c40d
SellerPrint <tretisgame999@gmail.com>
2026-03-13 11:33:29 +0100

    Remove duplicate service worker options

--- sw.js
+++ sw.js
@@ -4,10 +4,3 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10055383
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
